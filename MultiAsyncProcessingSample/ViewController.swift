f08b0a6891c1bc226687daa7eb713b51cbe8f77b
shtnkgm <nakagami.shota@gmail.com>
2017-06-22 00:33:14 +0900

    initial

--- MultiAsyncProcessingSample/ViewController.swift
+++ MultiAsyncProcessingSample/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        doMultiAsuncProcess()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func doMultiAsuncProcess() {
+        let dispatchGroup = DispatchGroup()
+        // 直列キュー / attibutes指定なし
+        //let dispatchQueue = DispatchQueue(label: "queue")
+        
+        // 並列キュー / attribute指定あり(.concurrent）
+        let dispatchQueue = DispatchQueue(label: "queue", attributes: .concurrent)
+        
+        for i in 1...5 {
+            dispatchGroup.enter()
+            dispatchQueue.async(group: dispatchGroup) {
+                [weak self] in
+                self?.asyncProcess(number: i) {
+                    (number: Int) -> Void in
+                    print("#\(number) End")
+                    dispatchGroup.leave()
+                }
+            }
+        }
+        
+        // 全ての非同期処理完了後にメインスレッドで処理
+        dispatchGroup.notify(queue: .main) {
+            print("All Process Done!")
+        }
+    }
+    
+    // 非同期処理
+    func asyncProcess(number: Int, completion: (_ number: Int) -> Void) {
+        print("#\(number) Start")
+        sleep((arc4random() % 100 + 1) / 100)
+        completion(number)
+    }
+    
 }
 
